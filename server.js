d0d4bfb027e29510e545d6a9e1786526d1555ba7
Nick Baughan <JNickBaughan@Gmail.com>
2018-01-17 18:44:21 -0500

    added first API route and updated H1

--- server.js
+++ server.js
@@ -4,4 +4,8 @@ let PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/src'));
 
+app.get('/todos', function(req, res){
+    res.send('API: all todos');
+})
+
 app.listen(PORT);
